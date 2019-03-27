5eaae14ab2b8cc98c1f383d537325c3781a902ba
Dinh Tien <hippp@Dinhs-MacBook-Pro.local>
2019-03-27 15:55:34 +0700

    add migration & seeder to fake data

--- database/seeds/ClassesTableSeeder.php
+++ database/seeds/ClassesTableSeeder.php
@@ -11,20 +11,6 @@ class ClassesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('classes')->insert(
-            ['class' => 'lớp 1'],
-            ['class' => 'lớp 2'],
-            ['class' => 'lớp 3'],
-            ['class' => 'lớp 4'],
-            ['class' => 'lớp 5'],
-            ['class' => 'lớp 6'],
-            ['class' => 'lớp 7'],
-            ['class' => 'lớp 8'],
-            ['class' => 'lớp 9'],
-            ['class' => 'lớp 10'],
-            ['class' => 'lớp 11'],
-            ['class' => 'lớp 12']
-        );
         for ($i = 1; $i < 13; $i++) {
             DB::table('classes')->insert(['class' => "lớp $i"]);
         }
